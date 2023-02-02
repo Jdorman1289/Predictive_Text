d355c9567b50de6797c114106ead06b428ee9237
Jesse Dorman <jdorman1289@gmail.com>
2023-02-02 17:04:04 -0600

    start working multiword/letter context

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import time
 import random
+from itertools import combinations
+ 
 
-#data = """Improving the robustness of machine learning models for natural language tasks has become a major artificial intelligence topic in recent years. Large language models have always been one of the most trending areas in AI research, backed by the rise of generative AI and companies racing to release architectures that can create impressively readable content, even computer code. Language models have traditionally been trained using online texts from sources such as Wikipedia, news stories, scientific papers and novels. However, in recent years, the tendency has been to train these models on increasing amounts of data in order to improve their accuracy and versatility. But, according to a team of AI forecasters, there is a concern on the horizon: we may run out of data to train them on. Researchers from Epoch emphasize in a study that high-quality data generally used for training language models may be depleted as early as 2026. As developers create more sophisticated models with superior capabilities, they must gather more texts to train them on, and LLM researchers are now increasingly concerned about running out of quality data. A principal research scientist in the MIT Information and Decision Systems laboratory and leader of the lab's Data-to-AI group, may have found the solution. In a paper on Rewrite and Rollback R&R: Metric-Guided Adversarial Sentence Generation recently published in the findings of AACL-IJCNLP 2022, the proposed framework can tweak and turn low-quality data (from sources such as Twitter and 4Chan) into high-quality data such as that from sources with editorial filters, such as Wikipedia and industry websites, increasing the amount of the correct type of data to test and train language models on. First, the data confirms that our average number of heads does approach what probability suggests it should be. Furthermore, this average improves with more trials. In 10 trials, there's some slight error, but this error almost disappears entirely with 1,000,000 trials. As we get more trials, the deviation away from the average decreases. Sound familiar? Sure, we could have flipped the coin ourselves, but Python saves us a lot of time by allowing us to model this process in code. As we get more and more data, the real-world starts to resemble the ideal."""
+data = """Improving the robustness of machine learning models for natural language tasks has become a major artificial intelligence topic in recent years. Large language models have always been one of the most trending areas in AI research, backed by the rise of generative AI and companies racing to release architectures that can create impressively readable content, even computer code. Language models have traditionally been trained using online texts from sources such as Wikipedia, news stories, scientific papers and novels. However, in recent years, the tendency has been to train these models on increasing amounts of data in order to improve their accuracy and versatility. But, according to a team of AI forecasters, there is a concern on the horizon: we may run out of data to train them on. Researchers from Epoch emphasize in a study that high-quality data generally used for training language models may be depleted as early as 2026. As developers create more sophisticated models with superior capabilities, they must gather more texts to train them on, and LLM researchers are now increasingly concerned about running out of quality data. A principal research scientist in the MIT Information and Decision Systems laboratory and leader of the lab's Data-to-AI group, may have found the solution. In a paper on Rewrite and Rollback R&R: Metric-Guided Adversarial Sentence Generation recently published in the findings of AACL-IJCNLP 2022, the proposed framework can tweak and turn low-quality data (from sources such as Twitter and 4Chan) into high-quality data such as that from sources with editorial filters, such as Wikipedia and industry websites, increasing the amount of the correct type of data to test and train language models on. First, the data confirms that our average number of heads does approach what probability suggests it should be. Furthermore, this average improves with more trials. In 10 trials, there's some slight error, but this error almost disappears entirely with 1,000,000 trials. As we get more trials, the deviation away from the average decreases. Sound familiar? Sure, we could have flipped the coin ourselves, but Python saves us a lot of time by allowing us to model this process in code. As we get more and more data, the real-world starts to resemble the ideal."""
 
-with open('WikiQA-train.txt','r') as file:
-    data = file.read()
+# with open('WikiQA-train.txt','r') as file:
+#     data = file.read()
 
 def total_next_outcomes(data, prompt):
     data_list_relationships = data.split()
+
+    combo_size = len(prompt.split())
+
+    data_list_relationships = list(combinations(data_list_relationships, combo_size))
+    data_list_relationships = [' '.join(i) for i in data_list_relationships]
+
     loop = 0
 
     next_words = []
@@ -34,7 +42,7 @@ def probable_next_word(possible_words):
         else:
             word_count[word] = 1
 
-    #print(word_count)
+    print(word_count)
     return word_count
 
 
