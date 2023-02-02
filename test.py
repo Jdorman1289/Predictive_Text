922d676390efd12b5ba0771429c0ca8712ade344
Jesse Dorman <Jesse.dorman@us.af.mil>
2023-02-02 20:11:00 +0000

    add logic for no probable choice

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 data = "First, the data confirms that our average number of heads does approach what probability suggests it should be. Furthermore, this average improves with more trials. In 10 trials, there's some slight error, but this error almost disappears entirely with 1,000,000 trials. As we get more trials, the deviation away from the average decreases. Sound familiar? Sure, we could have flipped the coin ourselves, but Python saves us a lot of time by allowing us to model this process in code. As we get more and more data, the real-world starts to resemble the ideal."
 
@@ -31,15 +32,22 @@ def probable_next_word(possible_words):
 
 
 def most_probable_word(word_count):
-    highest_count = 0
-    most_probable_word = ""
+    values = list(word_count.values())
+    is_same = all(x == values[0] for x in values)
 
-    for key, value in word_count.items():
-        if value > highest_count:
-            highest_count = value
-            most_probable_word = key
-      
-    return most_probable_word
+    if is_same:
+        return random.choice(list(word_count.keys()))
+    else:
+        highest_count = 0
+        most_probable_word = ""
+
+        for key, value in word_count.items():
+            if value > highest_count:
+                highest_count = value
+                most_probable_word = key
+        
+        return most_probable_word
+
 
 prompt = input("Prompt: ")
 
