df19f50c72533e937bd0600454a4a5e7b104f766
Jesse Dorman <Jesse.dorman@us.af.mil>
2023-02-02 18:39:04 +0000

    build out basic word guess probability

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 data = "First, the data confirms that our average number of heads does approach what probability suggests it should be. Furthermore, this average improves with more trials. In 10 trials, there's some slight error, but this error almost disappears entirely with 1,000,000 trials. As we get more trials, the deviation away from the average decreases. Sound familiar? Sure, we could have flipped the coin ourselves, but Python saves us a lot of time by allowing us to model this process in code. As we get more and more data, the real-world starts to resemble the ideal."
 
-prompt = input("Prompt: ")
-
 def total_next_outcomes(data, prompt):
     data_list_relationships = data.split()
     loop = 0
@@ -16,5 +14,37 @@ def total_next_outcomes(data, prompt):
 
     return next_words
 
+
+def probable_next_word(possible_words):
+    word_count = {}
+
+    for word in possible_words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    return word_count
+
+
+def most_probable_word(word_count):
+    highest_count = 0
+    most_probable_word = ""
+
+    for key, value in word_count.items():
+        if value > highest_count:
+            highest_count = value
+            most_probable_word = key
+
+    return most_probable_word
+
+
+prompt = input("Prompt: ")
+
 possible_words = total_next_outcomes(data, prompt)
 
+word_count = probable_next_word(possible_words)
+
+next_word = most_probable_word(word_count)
+
+print(next_word)
